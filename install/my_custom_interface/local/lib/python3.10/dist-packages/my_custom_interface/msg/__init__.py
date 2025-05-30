dc1a5ebfb76788455e36401f9461587a21137f76
Surya-2006-gif <suryaatphase2@gmail.com>
2025-05-30 18:28:29 +0530

    Delete install directory

--- install/my_custom_interface/local/lib/python3.10/dist-packages/my_custom_interface/msg/__init__.py
+++
@@ -1,1 +0,0 @@
-from my_custom_interface.msg._target_coordinates import TargetCoordinates  # noqa: F401
--- install/my_custom_interface/share/my_custom_interface/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(my_custom_interface_IDL_FILES "msg/TargetCoordinates.idl")
-set(my_custom_interface_INTERFACE_FILES "msg/TargetCoordinates.msg")
