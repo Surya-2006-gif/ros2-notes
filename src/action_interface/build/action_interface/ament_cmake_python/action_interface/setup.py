48782b243001de9ca4ca6fd2d2c5111199630e29
Surya-2006-gif <suryaatphase2@gmail.com>
2025-06-19 09:04:22 +0530

    Delete src directory

--- src/action_interface/build/action_interface/ament_cmake_python/action_interface/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages
-from setuptools import setup
-
-setup(
-    name='action_interface',
-    version='0.0.0',
-    packages=find_packages(
-        include=('action_interface', 'action_interface.*')),
-)
--- src/action_interface/install/action_interface/local/lib/python3.10/dist-packages/action_interface/action/__init__.py
+++
@@ -1,3 +0,0 @@
-from action_interface.action._number import Number  # noqa: F401
-from action_interface.action._shape import Shape  # noqa: F401
-from action_interface.action._trial import Trial  # noqa: F401
