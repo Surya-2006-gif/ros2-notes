b857c4bfe2dbf78b73dff7ecbbcad94f5e6f6c97
Surya-2006-gif <suryaatphase2@gmail.com>
2025-07-13 11:22:27 +0530

    Delete src/build directory

--- src/build/yolo/colcon_command_prefix_setup_py.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/surya/ros2_ws/src/install/yolov8_msgs/share/yolov8_msgs/package.sh"
--- src/build/yolov8_msgs/yolov8_msgs__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/surya/ros2_ws/src/build/yolov8_msgs/yolov8_msgs__py
-# Build directory: /home/surya/ros2_ws/src/build/yolov8_msgs/yolov8_msgs__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
