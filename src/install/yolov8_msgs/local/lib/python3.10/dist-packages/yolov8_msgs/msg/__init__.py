906c61c2c61fe5a77037b30162de644cfde22199
Surya-2006-gif <suryaatphase2@gmail.com>
2025-07-13 11:22:41 +0530

    Delete src/install directory

--- src/install/yolov8_msgs/local/lib/python3.10/dist-packages/yolov8_msgs/msg/__init__.py
+++
@@ -1,2 +0,0 @@
-from yolov8_msgs.msg._detections import Detections  # noqa: F401
-from yolov8_msgs.msg._yolo import Yolo  # noqa: F401
--- src/install/yolov8_msgs/share/yolov8_msgs/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(yolov8_msgs_IDL_FILES "msg/Yolo.idl;msg/Detections.idl")
-set(yolov8_msgs_INTERFACE_FILES "msg/Yolo.msg;msg/Detections.msg")
